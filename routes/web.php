c521ae49beb98a9d19753d1dbc252c1a7a19ea92
SnorlaxSPI <aflinsjr@icloud.com>
2025-10-18 18:05:57 -0300

    Aula 39

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobreNos');
+Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
 
 Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-//Route::get('/contato/{nome}', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::prefix('/app')->group(function () {
+  Route::get('/clientes', function () {
+    return 'Clientes';
+  })->name('app.clientes');
 
-Route::get(
-  '/contato/{nome}/{categoria_id}',
-  function (
-    string $nome = 'Desconhecido',
-    int $categoria_id = 1
-  ) {
-    echo "Estamos aqui: $nome - $categoria_id";
-  }
-)->where('categoria_id', '[0-9]+')
-  ->where('nome', '[A-Za-z]+')
-  ->name('site.contato');
+  Route::get('/fornecedores', function () {
+    return 'Fornecedores';
+  })->name('app.fornecedores');
+
+  Route::get('/produtos', function () {
+    return 'Produtos';
+  })->name('app.produtos');
+});
