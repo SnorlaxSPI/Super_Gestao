8992b4523f625d6e093382f7e9850a415fac987b
SnorlaxSPI <aflinsjr@icloud.com>
2025-10-18 12:30:45 -0300

    Aula 35

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,17 @@ Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])-
 Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobreNos');
 
 Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+
+//Route::get('/contato/{nome}', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+
+Route::get(
+  '/contato/{nome}/{categoria_id}',
+  function (
+    string $nome = 'Desconhecido',
+    int $categoria_id = 1
+  ) {
+    echo "Estamos aqui: $nome - $categoria_id";
+  }
+)->where('categoria_id', '[0-9]+')
+  ->where('nome', '[A-Za-z]+')
+  ->name('site.contato');
