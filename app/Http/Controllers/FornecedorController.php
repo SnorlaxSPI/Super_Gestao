f1a710ce43e36f701ec1439eaa58b2efb8bd96c3
SnorlaxSPI <aflinsjr@icloud.com>
2025-10-22 20:01:40 -0300

    Aula 47

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,6 +8,19 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            'Fornecedor 40',
+            'Fornecedor 41',
+            'Fornecedor 42',
+            'Fornecedor 43',
+            'Fornecedor 44',
+            'Fornecedor 45',
+            'Fornecedor 46',
+            'Fornecedor 47',
+            'Fornecedor 48',
+            'Fornecedor 49',
+            'Fornecedor 50',
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,8 +1,21 @@
 <h3>Fornecedores</h3>
 
-@@php
-use App\Models\Fornecedor;
+@php
+    /*
+  if () {
 
-$fornecedores = Fornecedor::all();
+  } elseif() {
 
+  } else {
+
+  }
+  */
 @endphp
+
+@if (count($fornecedores) >= 0 && count($fornecedores) < 10)
+    <h4>Existem poucos fornecedores cadastrados</h4>
+@elseif (count($fornecedores) >= 10 && count($fornecedores) < 50)
+    <h4>Existem vários fornecedores cadastrados</h4>
+@else
+    <h4>Existem muitos fornecedores cadastrados</h4>
+@endif
