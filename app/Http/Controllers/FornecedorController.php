6c8f480861cf87f8d2522df91ab76e460f202cca
SnorlaxSPI <aflinsjr@icloud.com>
2025-10-25 15:53:25 -0300

    Aula 53

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,27 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.000/0001-00', 'ddd' => '14', 'telefone' => '99999-9999'],
-            1 => ['nome' => 'Fornecedor 2', 'status' => 'S'],
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/0001-00',
+                'ddd' => '', // São Paulo
+                'telefone' => '99999-9999'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => '00.000.000/0001-01',
+                'ddd' => '14', // Bauru
+                'telefone' => '88888-8888'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '00.000.000/0001-02',
+                'ddd' => '16', // Ribeirão Preto
+                'telefone' => '77777-7777'
+            ],
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,9 +7,25 @@
 @isset($fornecedores)
     Fornecedor: {{ $fornecedores[0]['nome'] }} <br>
     Status: {{ $fornecedores[0]['status'] }} <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'CNPJ não informado' }} <br>
-    @endisset
+    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'CNPJ não informado' }} <br>
+    Telefone: ({{ $fornecedores[0]['ddd'] ?? 'DDD não informado' }})
+    {{ $fornecedores[0]['telefone'] ?? 'Telefone não informado' }} <br>
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+        @break
+
+        @case('14')
+            Bauru - SP
+        @break
+
+        @case('16')
+            Ribeirão Preto - SP
+        @break
+
+        @default
+            Localidade não encontrada -
+    @endswitch
 @endisset
 
 @if ($fornecedores[0]['status'] == 'S')
