4b707fc8b69b5d4a405b73db767ac5dce05b9a9b
SnorlaxSPI <aflinsjr@icloud.com>
2025-10-25 16:03:42 -0300

    Aula 54

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -5,35 +5,14 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }} <br>
-    Status: {{ $fornecedores[0]['status'] }} <br>
-    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'CNPJ não informado' }} <br>
-    Telefone: ({{ $fornecedores[0]['ddd'] ?? 'DDD não informado' }})
-    {{ $fornecedores[0]['telefone'] ?? 'Telefone não informado' }} <br>
-    @switch($fornecedores[0]['ddd'])
-        @case('11')
-            São Paulo - SP
-        @break
+    <p><b>Listagem de fornecedores</b></p>
 
-        @case('14')
-            Bauru - SP
-        @break
-
-        @case('16')
-            Ribeirão Preto - SP
-        @break
-
-        @default
-            Localidade não encontrada -
-    @endswitch
+    @for ($i = 0; isset($fornecedores[$i]); ++$i)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }} <br>
+        Status: {{ $fornecedores[$i]['status'] }} <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'CNPJ não informado' }} <br>
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? 'DDD não informado' }})
+        {{ $fornecedores[$i]['telefone'] ?? 'Telefone não informado' }} <br>
+        <hr>
+    @endfor
 @endisset
-
-@if ($fornecedores[0]['status'] == 'S')
-    Fornecedor Ativo
-@else
-    Fornecedor Inativo
-@endif
-<br>
-@unless ($fornecedores[1]['status'] == 'S')
-    Fornecedor Inativo no unless
-@endunless
