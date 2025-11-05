a227ee58aea5a2ee683bb90e2c9fb74e5f59f796
SnorlaxSPI <aflinsjr@icloud.com>
2025-11-05 19:24:40 -0300

    Aula 67

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        //var_dump($_GET);
+        var_dump($_GET);
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
