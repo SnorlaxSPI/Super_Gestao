e69c970467c01d151f0af4973b9cf34d2b72f1aa
SnorlaxSPI <aflinsjr@icloud.com>
2025-11-05 18:55:14 -0300

    Aula 67

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -9,8 +9,8 @@
 </head>
 
 <body>
-
+    @include('site.layouts._partials.topo')
+    @yield('conteudo')
 </body>
-@yield('conteudo')
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\TesteController;
 
-Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
-Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return 'Olá, seja bem vindo ao curso!';
+});
+*/
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function () {
+  return 'Login';
+})->name('site.login');
 
 Route::prefix('/app')->group(function () {
   Route::get('/clientes', function () {
     return 'Clientes';
   })->name('app.clientes');
-
-  Route::get('/fornecedores', [App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedores');
-
+  Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
   Route::get('/produtos', function () {
-    return 'Produtos';
+    return 'produtos';
   })->name('app.produtos');
 });
 
-Route::get('/rota1', function () {
-  //return redirect()->route('site.index');
-  echo 'Rota 1';
-})->name('site.rota1');
-
-Route::get('/rota2', function () {
-  return redirect()->route('site.rota1');
-  //echo 'Rota 2';
-})->name('site.rota2');
-
-Route::get('/teste/{p1}/{p2}', [App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.teste');
 
 Route::fallback(function () {
-  echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a página inicial.';
+  echo 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para página inicial';
 });
