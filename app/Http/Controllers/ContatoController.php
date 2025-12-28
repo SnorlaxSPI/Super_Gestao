b97a2a0ded017f4603aedee9e8c4bc6e1d8b6d08
SnorlaxSPI <aflinsjr@icloud.com>
2025-12-28 10:28:13 -0300

    123

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        var_dump($_POST);
+        return view('site.contato', ['titulo' => 'Contato']);
+    }
+
+    public function salvar(Request $request)
+    {
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+
+        // $contato->save();
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
--- database/migrations/2025_11_15_201002_create_site_contatos_table.php
+++ database/migrations/2025_11_15_201002_create_site_contatos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('nome', 50);
+            $table->string('telefone', 20);
             $table->string('email', 80);
             $table->integer('motivo_contato');
             $table->text('mensagem');
